18bebec738285187bf5331dd050e1e9c751f6bfc
IsaqueTADS-Linux <isauqeofc18@gmail.com>
2026-04-20 21:57:17 -0300

    feat: update experience page with new sections

--- src/app/(portfolio)/experience/page.tsx
+++ src/app/(portfolio)/experience/page.tsx
@@ -1,7 +1,19 @@
+import { ExperienceHero } from "./_components/experience-hero"
+import { ExperienceTimeline } from "./_components/experience-timeline"
+import { EducationTimeline } from "./_components/education-timeline"
+import { CertificatesTimeline } from "./_components/certificates-timeline"
+import { SkillsSection } from "./_components/skills-section"
+
 export default function Page() {
   return (
-    <div>
-      <h1>Experience</h1>
+    <div className="flex px-4">
+      <div className="flex w-full flex-col gap-16">
+        <ExperienceHero />
+        <EducationTimeline />
+        <CertificatesTimeline />
+        <ExperienceTimeline />
+        <SkillsSection />
+      </div>
     </div>
   )
 }
